a42227ab532c77a72e5aa01c680b6acec98d6acb
Yvaral Louis-Jame <yvl905@g.harvard.edu>
2022-02-25 04:43:58 +0000

    chang

--- practice/form-a/index.php
+++ practice/form-a/index.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_SESSION['results'])) {
     $results =$_SESSION['results'];
     $inputSring = $results['inputString'];
-    $isPalindrome = $results['isPalindrome'];
+    $isPalindrome = $results['inputString'];
     $vowelCount = $results['vowelCount'];
 
     $_SESSION['results'] = null;

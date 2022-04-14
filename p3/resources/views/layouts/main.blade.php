58788e6d18b9b997ad803ae5db8f11d4233927f4
Yvaral Louis-Jame <yvl905@g.harvard.edugit config --global user.name Yvaral>
2022-04-14 20:23:47 +0000

    New-changes

---
+++ p3/resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>@yield('title', 'Alumni Circle')</title>
+    <meta charset='utf-8'>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href='/css/alumni.css' type='text/css' rel='stylesheet'>
+
+    @yield('head')
+</head>
+<body>
+
+    <header>
+        <a href='/'><img src='/images/alumni-logo@2x.png' id='logo' alt='Alumni Logo'></a>
+          <h1>HYDE PARK UNIVERSITY</h>
+              <h3>Alumni Circle</h3>
+        <nav>
+            <ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/members'>Members</a></li>
+                <li><a href='/about'>About Us</a></li>
+                <li><a href='/contact'>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id='main'>
+        @yield('content')
+    </section>
+
+    <footer>
+        &copy; Alumni Circle, Inc.
+        {{config('mail.contact_email')}}
+    </footer>
+
+</body>
+</html>
+
